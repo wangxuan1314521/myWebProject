66ad20df27614addf525deeedeea15728b2ed224
wangxuan <3154263578@qq.com>
2023-02-18 19:52:55 +0800

    我的第五次web提交 修改功能实现

---
+++ webproject/src/main/java/com/itheima/web/UpdateServlet.java
@@ -0,0 +1,52 @@
+package com.itheima.web;
+
+import com.itheima.pojo.Brand;
+import com.itheima.service.BrandService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author shkstart
+ * @create 2023-02-18 19:44
+ */
+@WebServlet(name = "updateServlet")
+public class UpdateServlet extends HttpServlet {
+
+    private BrandService brandService = new BrandService();
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String id = request.getParameter("id");
+
+        String brandName = request.getParameter("brandName");
+        String companyName = request.getParameter("companyName");
+        String ordered = request.getParameter("ordered");
+        String description = request.getParameter("description");
+        String status = request.getParameter("status");
+
+        Brand brand = new Brand();
+        brand.setId(Integer.parseInt(id));
+        brand.setBrandName(brandName);
+        brand.setDescription(description);
+        brand.setStatus(Integer.parseInt(status));
+        brand.setCompanyName(companyName);
+        brand.setOrdered(Integer.parseInt(ordered));
+
+
+        brandService.update(brand);
+         //转发到查询所有的servlet页面
+        request.getRequestDispatcher("/SelectAllServlet").forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+
+
+}
